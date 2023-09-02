569e0528c48290def590842942e64e3d892fd68d
Piseth <piseth_lee@yahoo.com>
2023-09-03 04:08:09 +0700

    chore: remove unused functions

--- src/utils/string.ts
+++
@@ -1,6 +0,0 @@
-export function fuzzyMatchRegExp(searchPattern: string, text: string): boolean {
-  const fuzzyPattern =
-    '.*' + searchPattern.toLowerCase().split('').join('.*') + '.*'
-  const re = new RegExp(fuzzyPattern, 'i')
-  return !!re.test(text.toLowerCase())
-}
