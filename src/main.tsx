4a3c2f252709def09c0beb1eedc64698ad9a97cd
Piseth <piseth_lee@yahoo.com>
2023-09-02 10:41:59 +0700

    fix: set BrowserRoute basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/countries-catalog/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route
